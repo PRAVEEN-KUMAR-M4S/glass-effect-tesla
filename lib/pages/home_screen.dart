2787cb411170f82e8da7ade6b564da268f95c5a3
PraveenKumar S <praveenkumar@katenterprise.com>
2026-09-13 15:12:02 +0530

    battery info

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -3,11 +3,72 @@ import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import 'package:svg_flutter/svg.dart';
 import 'package:tesla_animation/controller/home_controller.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final HomeController homeController;
   const HomeScreen({super.key, required this.homeController});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   /// final HomeController _homeController = HomeController();
+  ///
+
+  late AnimationController _batteryController;
+  late Animation<double> _batteryAnimation;
+  late Animation<Offset> _batteryStatusAnimation;
+
+  int _previousIndex = 0;
+
+  void setupBatteryAnimation() {
+    _batteryController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+
+    _batteryAnimation = CurvedAnimation(
+      parent: _batteryController,
+      curve: Interval(0.0, 0.5),
+    );
+
+    _batteryStatusAnimation = _batteryStatusAnimation =
+        Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero).animate(
+          CurvedAnimation(
+            parent: _batteryController,
+            curve: const Interval(0.6, 1.0, curve: Curves.easeOut),
+          ),
+        );
+  }
+
+  void _handleTabChange() {
+    final currentIndex = widget.homeController.selectedIndex;
+
+    // Battery tab
+    if (currentIndex == 1 && _previousIndex != 1) {
+      _batteryController.forward(from: 0);
+    } else if (currentIndex != 1 && _previousIndex == 1) {
+      _batteryController.reverse();
+    }
+
+    _previousIndex = currentIndex;
+  }
+
+  @override
+  void initState() {
+    setupBatteryAnimation();
+    widget.homeController.addListener(_handleTabChange);
+    _previousIndex = widget.homeController.selectedIndex;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _batteryController.dispose();
+    widget.homeController.removeListener(_handleTabChange);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +87,10 @@ class HomeScreen extends StatelessWidget {
 
       body: SafeArea(
         child: AnimatedBuilder(
-          animation: homeController,
+          animation: Listenable.merge([
+            _batteryController,
+            widget.homeController,
+          ]),
           builder: (context, snapshot) {
             return LayoutBuilder(
               builder: (context, constrains) {
@@ -35,7 +99,7 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Padding(
                       padding: EdgeInsets.symmetric(
-                        vertical: constrains.maxWidth * 0.4,
+                        vertical: constrains.maxWidth * 0.3,
                       ),
                       child: SvgPicture.asset(
                         "assets/icons/Car.svg",
@@ -44,63 +108,134 @@ class HomeScreen extends StatelessWidget {
                     ),
 
                     AnimatedPositioned(
-                      right: homeController.selectedIndex == 0
+                      right: widget.homeController.selectedIndex == 0
                           ? constrains.maxWidth * 0.1
                           : constrains.maxWidth / 2.2,
                       duration: Duration(milliseconds: 300),
                       child: AnimatedOpacity(
                         duration: Duration(milliseconds: 300),
-                        opacity: homeController.selectedIndex == 0 ? 1 : 0,
+                        opacity: widget.homeController.selectedIndex == 0
+                            ? 1
+                            : 0,
                         child: DoorLock(
-                          onTap: () =>
-                              homeController.updateDoorStatus(Door.right),
-                          status: homeController.isDoorLocked(Door.right),
+                          onTap: () => widget.homeController.updateDoorStatus(
+                            Door.right,
+                          ),
+                          status: widget.homeController.isDoorLocked(
+                            Door.right,
+                          ),
                         ),
                       ),
                     ),
 
                     AnimatedPositioned(
-                      left: homeController.selectedIndex == 0
+                      left: widget.homeController.selectedIndex == 0
                           ? constrains.maxWidth * 0.1
                           : constrains.maxWidth / 2.2,
                       duration: Duration(milliseconds: 300),
                       child: AnimatedOpacity(
                         duration: Duration(milliseconds: 300),
-                        opacity: homeController.selectedIndex == 0 ? 1 : 0,
+                        opacity: widget.homeController.selectedIndex == 0
+                            ? 1
+                            : 0,
                         child: DoorLock(
                           onTap: () =>
-                              homeController.updateDoorStatus(Door.left),
-                          status: homeController.isDoorLocked(Door.left),
+                              widget.homeController.updateDoorStatus(Door.left),
+                          status: widget.homeController.isDoorLocked(Door.left),
                         ),
                       ),
                     ),
                     AnimatedPositioned(
-                      top: homeController.selectedIndex == 0
-                          ? constrains.maxWidth * 0.5
+                      top: widget.homeController.selectedIndex == 0
+                          ? constrains.maxWidth * 0.4
                           : constrains.maxHeight / 2,
                       duration: Duration(milliseconds: 300),
                       child: AnimatedOpacity(
                         duration: Duration(milliseconds: 300),
-                        opacity: homeController.selectedIndex == 0 ? 1 : 0,
+                        opacity: widget.homeController.selectedIndex == 0
+                            ? 1
+                            : 0,
                         child: DoorLock(
-                          onTap: () =>
-                              homeController.updateDoorStatus(Door.front),
-                          status: homeController.isDoorLocked(Door.front),
+                          onTap: () => widget.homeController.updateDoorStatus(
+                            Door.front,
+                          ),
+                          status: widget.homeController.isDoorLocked(
+                            Door.front,
+                          ),
                         ),
                       ),
                     ),
                     AnimatedPositioned(
-                      bottom: homeController.selectedIndex == 0
-                          ? constrains.maxWidth * 0.5
+                      bottom: widget.homeController.selectedIndex == 0
+                          ? constrains.maxWidth * 0.4
                           : constrains.maxHeight / 2,
                       duration: Duration(milliseconds: 300),
                       child: AnimatedOpacity(
                         duration: Duration(milliseconds: 300),
-                        opacity: homeController.selectedIndex == 0 ? 1 : 0,
+                        opacity: widget.homeController.selectedIndex == 0
+                            ? 1
+                            : 0,
                         child: DoorLock(
                           onTap: () =>
-                              homeController.updateDoorStatus(Door.back),
-                          status: homeController.isDoorLocked(Door.back),
+                              widget.homeController.updateDoorStatus(Door.back),
+                          status: widget.homeController.isDoorLocked(Door.back),
+                        ),
+                      ),
+                    ),
+
+                    // AnimatedOpacity(
+                    //   duration: const Duration(milliseconds: 400),
+                    //   opacity: widget.homeController.selectedIndex == 1
+                    //       ? 1.0
+                    //       : 0.0,
+                    //   child: SvgPicture.asset(
+                    //     "assets/icons/Battery.svg",
+                    //     width: constrains.maxWidth * 0.4,
+                    //   ),
+                    // ),
+                    Opacity(
+                      opacity: _batteryAnimation.value,
+                      child: SvgPicture.asset(
+                        "assets/icons/Battery.svg",
+                        width: constrains.maxWidth * 0.4,
+                      ),
+                    ),
+
+                    SlideTransition(
+                      position: _batteryStatusAnimation,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          bottom: constrains.minHeight * 0.12,
+                          top: 30,
+                          left: 8,
+                          right: 8,
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              "230 km",
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            Text("54 %", style: TextStyle(fontSize: 24)),
+                            Spacer(),
+                            Text("CHARGING", style: TextStyle(fontSize: 20)),
+                            Text(
+                              "19 mins remaing",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            SizedBox(height: constrains.minHeight * 0.04),
+                            DefaultTextStyle(
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [Text("22 km/hr"), Text("245 v")],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
